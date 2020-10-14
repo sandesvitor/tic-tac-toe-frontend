34a85cd8ddeb9552274c67e287138312bc68ccb9
Vitor Sandes <sandesvitor@gmail.com>
2020-10-14 16:47:04 -0300

    Testing connections with sockets

--- src/test/test.js
+++ src/test/test.js
@@ -1,11 +1,19 @@
 import { io } from '../resources/socketClient'
 
-const socket = io("http://localhost:5000/")
+const socket = io.connect("http://localhost:5000/")
 
 socket.on("connect", () => {
     console.log("Estamos conectados!")
 
-    socket.on("data", data => {
-        console(data)
+    socket.on("whoAmI", position => {
+        const msg = position !== 0
+            ? `You are Player ${position}`
+            : "Waiting for vacancy\nPlease wait..."
+        console.log("You are Player [%s]", position)
     })
+
+    socket.on("roomData", roomData => {
+        console.log(roomData)
+    })
+
 })
